176c1f3434098134d18cc145a2c365ccbd7ed022
andrefangeloni <andrefangeloni@gmail.com>
2022-02-14 00:55:16 -0300

    feat: models and routes users

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import { User } from '../models/User';
 
 class UserController {
   async create(request: Request, response: Response) {
-    const { body } = request;
-    console.log(body);
+    const { name, email } = request.body;
 
-    return response.json(body);
+    const usersRepository = getRepository(User);
+
+    const foundUserByEmail = await usersRepository.findOne({ email });
+
+    if (foundUserByEmail) {
+      return response.status(400).json({ error: 'email-already-using' });
+    }
+
+    const user = usersRepository.create({ name, email });
+
+    await usersRepository.save(user);
+
+    return response.json(user);
   }
 }
 
