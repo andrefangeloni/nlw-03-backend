ff400a53a2170ef91471d3d8213cd29319b567d9
andrefangeloni <andrefangeloni@gmail.com>
2022-02-18 09:05:27 -0300

    feat: surveyUser

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,19 @@ import { Router } from 'express';
 
 import { UserController } from './controllers/UserController';
 import { SurveyController } from './controllers/SurveyController';
+import { SendEmailController } from './controllers/SendEmailController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
+const sendEmailController = new SendEmailController();
 
 router.post('/users', userController.create);
 
 router.get('/surveys', surveyController.show);
 router.post('/surveys', surveyController.create);
+
+router.post('/sendEmail', sendEmailController.execute);
 
 export { router };
